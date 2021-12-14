87bb82003b411de57ea2c024e6e38ac7782a7bf0
topinfrassi01 <francis.toupin@hotmail.com>
2021-12-14 12:18:29 -0500

    change code to get multiple seasons

--- scraper.py
+++ scraper.py
@@ -5,29 +5,17 @@ import math
 import csv
 from pathlib import Path
 
-csv_header = ["#", "Player", "Season", "Team", "Shoots", "Pos", "GP", "G", "A", "P", "+/-", "PIM", "P/GP", "EVG", "EVP",
+CSV_HEADERS = ["#", "Player", "Season", "Team", "Shoots", "Pos", "GP", "G", "A", "P", "+/-", "PIM", "P/GP", "EVG", "EVP",
               "PPG", "PPP", "SHG", "SHP", "OTG", "GWG",
               "S", "S%", "TOI/GP", "FOW%"]
 
-
-def scrape_nhl_standings(csv_dump_path, start_year, end_year):
+def scrape_nhl_standings(csv_dump_path, season):
     try:
         driver = webdriver.Chrome()
 
-        for year in range(start_year, end_year + 1):
+        for year in range(season[0], season[1]):
             driver.get(build_url(year))
 
-            # # There is a weird bug on the NHL website where the request for 07-08 season also returns
-            # # the 06-07 so we set it back.
-            # if year == 2007:
-            #     year_range = Select(driver.find_element(".filter__range").find_element_by_tag_name("select"))
-            #     year_range.select_by_value("20072008")
-            #     driver.find_element(".go").click()
-
-            #     # Force Selenium to wait for the page to reload
-            #     driver.implicitly_wait(5)
-
-            # We save about 2 seconds by setting the page size to 100.
             set_page_size_to_100(driver)
 
             players_standings = []
@@ -70,39 +58,31 @@ def set_page_size_to_100(driver):
 
 def parse_standings_page(standings):
     players_standings = []
-    cells_per_row = 23
     cells = standings.split('\n')
 
-    # There's a problem with markup here, below // isn't a comment
-    rows_count = len(cells) // cells_per_row
+    rows_count = len(cells) // len(CSV_HEADERS)
 
     if not rows_count - math.floor(rows_count) == 0:
         raise ValueError("Cells count isn't divisible by cells per row.")
 
     for i in range(0, int(rows_count)):
-        players_standings.append(cells[i * len(csv_header): (i + 1) * len(csv_header)])
+        players_standings.append(cells[i * len(CSV_HEADERS): (i + 1) * len(CSV_HEADERS)])
 
     return players_standings
-
-
-def try_parse_float(x):
-    return float(x) if not x == "--" else 0.
 
 
 def write_to_csv(csv_dump_path, players_standings, year):
     with open(str(csv_dump_path / "{0}-{1}.csv".format(year, year+1)), "w+", newline='') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=",")
-        csvwriter.writerow(csv_header)
-
-        for row in players_standings:
-            csvwriter.writerow(row)
+        csvwriter.writerow(CSV_HEADERS)
+        csvwriter.writerows(players_standings)
 
 
 if __name__ == "__main__":
-
     csv_path = Path(__file__).parent / 'statistics'
 
     if not csv_path.exists():
         csv_path.mkdir()
 
-    scrape_nhl_standings(csv_path, start_year=2017, end_year=2017)
+    for i in range(2015,2022):
+        scrape_nhl_standings(csv_path, season=(i, i+1))
