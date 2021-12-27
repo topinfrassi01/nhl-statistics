de570ad8db3ea547cd541c7fa7b63aeb678756b4
francis <francis.toupin.1@ens.etsmtl.ca>
2021-12-27 16:18:30 -0500

    Added similar player finder

---
+++ data_preparation/prepare_data_knn.py
@@ -0,0 +1,41 @@
+import pandas
+from typing import Sequence
+
+
+def drop_columns(df: pandas.DataFrame, columns: Sequence[str]) -> pandas.DataFrame:
+    for col in columns:
+        df = df.drop(col, axis=1)
+
+    return df
+
+
+def normalize_per_gp(df: pandas.DataFrame, columns: Sequence[str]) -> pandas.DataFrame:
+    games_played: int = df["GP"].astype(float)
+
+    for col in columns:
+        df[f"{col}/GP"] = df[col].astype(float) / games_played
+
+    df = drop_columns(df, columns)
+
+    return df
+
+
+def prepare(df: pandas.DataFrame) -> pandas.DataFrame:
+    df = df[df["S%"] != '--']
+    df = df[df["P"] != 0]
+    df = df[df["GP"] > 4]
+
+    df["EVA"] = df["EVP"] - df["EVG"]
+    df["PPA"] = df["PPP"] - df["PPG"]
+
+    df["Forward"] = df["Pos"].isin(['C', 'L', 'R']).astype(int)
+    df["Def"] = (df["Pos"] == 'D').astype(int)
+
+    df = normalize_per_gp(df, ["EVA", "EVG", "PPA", "PPG", "S"])
+    df["S%"] = df["S%"].astype(float) / 100
+
+    df = drop_columns(df, ["Season", "#", "FOW%", "G", "A", "P", "Shoots",
+                           "Pos", "EVP", "SHP", "PPP", "P/GP", "OTG", "GWG",
+                           "Team", "TOI/GP", "GP", "SHG",  "+/-", "PIM"])
+
+    return df
