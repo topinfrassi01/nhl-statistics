cbaf0337dbf1c1576353d1933e2f187f438915d1
francis <francis.toupin.1@ens.etsmtl.ca>
2022-01-02 21:44:52 -0500

    Aggregate yearly stats and get nearest comparables

--- data_preparation/prepare_data_knn.py
+++ data_preparation/prepare_data_knn.py
@@ -22,17 +22,22 @@ def normalize_per_gp(df: pandas.DataFrame, columns: Sequence[str]) -> pandas.Dat
 
 def prepare(df: pandas.DataFrame) -> pandas.DataFrame:
     df = df[df["S%"] != '--']
-    df = df[df["P"] != 0]
-    df = df[df["GP"] > 4]
+    #df = df[df["P"] != 0]
+    #df = df[df["GP"] > 4]
 
     df["EVA"] = df["EVP"] - df["EVG"]
     df["PPA"] = df["PPP"] - df["PPG"]
 
     df["Forward"] = df["Pos"].isin(['C', 'L', 'R']).astype(int)
-    df["Def"] = (df["Pos"] == 'D').astype(int)
+    #df["Def"] = (df["Pos"] == 'D').astype(int)
 
     df = normalize_per_gp(df, ["EVA", "EVG", "PPA", "PPG", "S"])
+    #df = normalize_per_gp(df, ["S"])
     df["S%"] = df["S%"].astype(float) / 100
+
+    #df = drop_columns(df, ["Season", "#", "FOW%", "G", "A", "P", "Shoots",
+    #                       "Pos", "EVP", "SHP", "PPP", "OTG", "GWG", "EVG",  "PPG",
+    #                       "Team", "TOI/GP", "GP", "SHG",  "+/-", "PIM"])
 
     df = drop_columns(df, ["Season", "#", "FOW%", "G", "A", "P", "Shoots",
                            "Pos", "EVP", "SHP", "PPP", "P/GP", "OTG", "GWG",
